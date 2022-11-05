96de3b26d6ecf210da847c0b48f8298534a9ae27
The-Estonian <Zaar2213@gmail.com>
2022-11-05 21:59:24 +0200

    created Rating, Product, Footer modules

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -1,14 +1,23 @@
-import React from 'react'
-import Card from "./Card"
+import React from 'react';
+import styles from './Product.module.css';
+import Rating from './Rating';
 
 const Product = (props) => {
   return (
-    <Card>
-        <a href={`/product/${props.product._id}`}>
-            <img src={props.product.image} alt={props.product.name}/>
-        </a>
-    </Card>
-  )
-}
+    <div className={styles.product}>
+      <a href={`/product/${props.product._id}`}>
+        <img src={props.product.image} alt={props.product.name} />
+        <h4>{props.product.name}</h4>
+      </a>
+      {/* <h5>{props.product.rating} from {props.product.numReviews} reviews</h5> */}
+      <Rating
+        value={props.product.rating}
+        text={`${props.product.numReviews} reviews`}
+        color={'#f8e825'}
+      />
+      <h3>${props.product.price}</h3>
+    </div>
+  );
+};
 
-export default Product
+export default Product;
